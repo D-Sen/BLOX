37e737ea4fad5a10e021310d10c677d953a4cb83
msiller455 <msiller455@gmail.com>
2023-01-20 14:57:34 -0800

    adds ensureLoggedIn middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const reviewsCtrl = require('../controllers/reviews')
 
 router.post('/movies/:id/reviews', reviewsCtrl.create)
+router.delete('/reviews/:id', reviewsCtrl.delete)
 
 
 module.exports = router
