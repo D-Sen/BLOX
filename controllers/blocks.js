a92f9374d6aadd213370ca751650317126d5942b
Domonic S <dsen@mail.com>
2023-01-26 21:08:25 -0800

    Update and Delete conformed as working, comments, old code removed.

--- controllers/blocks.js
+++ controllers/blocks.js
@@ -5,35 +5,58 @@ module.exports = {
   show,
   new: newBlock,
   create,
+  update,
+  edit
 };
 
+function update(req, res, next) {
+    const updateBlock = {
+        colorRed: req.body.colorRed,
+        colorGreen: req.body.colorGreen,
+        colorBlue: req.body.colorBlue
+    }
+    console.log('about to do a lot');
+    Block.findByIdAndUpdate(req.params.id, updateBlock, { new: true}, function(err, block){
+        if (err) {return next(err);} 
+        res.redirect(`/blocks/${block._id}`);
+    });
+}
+
+function edit(req, res, next) {
+    Block.findById(req.params.id, function(err, block){
+        if (err) {return next(err);}
+        res.render('blocks/edit', { title: "Block Detail", block});
+    });
+}
+
+
+
+
 function index(req, res) {
-  Block.find({}, function (err, blocks) {
-    res.render("blocks/index", { title: "All Blocks", blocks });
-  });
+    Block.find({}, function (err, blocks) {
+        res.render("blocks/index", { title: "All Blocks", blocks });
+    });
 }
 
 function show(req, res) {
-  Block.findById(req.params.id)
+    Block.findById(req.params.id)
     .exec(function (err, block) {
-        console.log(block);
         res.render("blocks/show", { title: "Block Detail", block});
     });
 }
 
 function newBlock(req, res) {
-  res.render("blocks/new", { title: "Add Block" });
+    res.render("blocks/new", { title: "Add Block" });
 }
 
 function create(req, res) {
-  // Delete empty properties on req.body for defaults to happen
-  for (let key in req.body) {
-    if (req.body[key] === "") delete req.body[key];
-  }
-  const block = new Block(req.body);
-  block.save(function (err) {
-    if (err) return res.redirect("/blocks/new");
-    console.log(block);
-    res.redirect(`/blocks/${block._id}`);
-  });
+// Delete empty properties on req.body for defaults to happen
+    for (let key in req.body) {
+        if (req.body[key] === "") delete req.body[key];
+    }
+    const block = new Block(req.body);
+    block.save(function (err) {
+        if (err) return res.redirect("/blocks/new");
+        res.redirect(`/blocks/${block._id}`);
+    });
 }
--- routes/blocks.js
+++ routes/blocks.js
@@ -8,5 +8,7 @@ router.get('/', blocksCtrl.index);
 router.get('/new', ensureLoggedIn, blocksCtrl.new);
 router.get('/:id', blocksCtrl.show);
 router.post('/', ensureLoggedIn, blocksCtrl.create);
+router.get('/:id/edit', ensureLoggedIn, blocksCtrl.edit)
+router.put('/:id', ensureLoggedIn, blocksCtrl.update);
 
 module.exports = router;
